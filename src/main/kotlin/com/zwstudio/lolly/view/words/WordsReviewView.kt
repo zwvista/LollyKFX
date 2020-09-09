be276b9bf678b7105476417926a873c88d75db1d
zwvista <zwvista@msn.com>
2020-09-09 13:39:11 +0900

    review

--- src/main/kotlin/com/zwstudio/lolly/view/words/WordsReviewView.kt
+++ src/main/kotlin/com/zwstudio/lolly/view/words/WordsReviewView.kt
@@ -4,6 +4,7 @@ import com.zwstudio.lolly.data.misc.ReviewOptionsViewModel
 import com.zwstudio.lolly.data.words.WordsReviewViewModel
 import com.zwstudio.lolly.view.ILollySettings
 import com.zwstudio.lolly.view.misc.ReviewOptionsView
+import javafx.scene.layout.Priority
 import tornadofx.*
 
 class WordsReviewView : Fragment("Words Review"), ILollySettings {
@@ -21,9 +22,57 @@ class WordsReviewView : Fragment("Words Review"), ILollySettings {
             button("Speak")
         }
         gridpane {
+            vgrow = Priority.ALWAYS
             paddingAll = 10.0
             hgap = 10.0
             vgap = 10.0
+            constraintsForRow(0).percentHeight = 12.5
+            constraintsForRow(1).percentHeight = 12.5
+            constraintsForRow(2).percentHeight = 50.0
+            constraintsForRow(3).percentHeight = 12.5
+            constraintsForRow(4).percentHeight = 12.5
+            constraintsForColumn(0).hgrow = Priority.ALWAYS
+            row {
+                hbox {
+                    label(vm.indexString) {
+                        visibleWhen(vm.indexIsVisible)
+                    }
+                    stackpane {
+                        label("Correct") {
+                            visibleWhen(vm.correctIsVisible)
+                        }
+                        label("Incorrect") {
+                            visibleWhen(vm.incorrectIsVisible)
+                        }
+                    }
+                    label(vm.accuracyString) {
+                        visibleWhen(vm.accuracyIsVisible)
+                    }
+                }
+            }
+            row {
+                hbox {
+                    label(vm.wordTargetString) {
+                        visibleWhen(vm.wordTargetIsVisible)
+                    }
+                    label(vm.noteTargetString) {
+                        visibleWhen(vm.noteTargetIsVisible)
+                    }
+                }
+            }
+            row {
+                textarea(vm.translationString) {
+
+                }
+            }
+            row {
+                textfield(vm.wordInputString)
+            }
+            row {
+                button(vm.checkString).action {
+                    vm.check()
+                }
+            }
         }
     }
 
