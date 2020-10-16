8a637a798eca53a8dd25663bf5a1a93294e7957c
zwvista <zwvista@msn.com>
2020-10-16 15:26:03 +0900

    intermediate

--- src/main/kotlin/com/zwstudio/lolly/data/dicts/TransformEditViewModel.kt
+++ src/main/kotlin/com/zwstudio/lolly/data/dicts/TransformEditViewModel.kt
@@ -15,21 +15,21 @@ class TransformEditViewModel(val vmDetail: DictsDetailViewModel) : ViewModel() {
     val sourceText = SimpleStringProperty("")
     val resultText = SimpleStringProperty("")
     var resultHtml = ""
-    val interimText = SimpleStringProperty("")
-    var interimMaxIndex = 0
-    val interimIndex = SimpleObjectProperty<Int>()
+    val intermediateText = SimpleStringProperty("")
+    var intermediateMaxIndex = 0
+    val intermediateIndex = SimpleObjectProperty<Int>()
     val lstTranformItems = toTransformItems(vmDetail.transform.value).asObservable()
-    var interimResults = mutableListOf<String>()
+    var intermediateResults = mutableListOf<String>()
 
     init {
-        interimIndex.addListener { _, _, newValue ->
-            interimText.value = interimResults[newValue]
+        intermediateIndex.addListener { _, _, newValue ->
+            intermediateText.value = intermediateResults[newValue]
         }
     }
 
     override fun onCommit() {
         super.onCommit()
-        vmDetail.transform.value = lstTranformItems.flatMap { listOf(it.extractor, it.replacement) }.joinToString("\n")
+        vmDetail.transform.value = lstTranformItems.flatMap { listOf(it.extractor, it.replacement) }.joinToString("\r\n")
     }
 
     fun getHtml(): Observable<Unit> {
@@ -39,16 +39,25 @@ class TransformEditViewModel(val vmDetail: DictsDetailViewModel) : ViewModel() {
 
     fun transformText() {
         var text = sourceText.value
-        interimResults = mutableListOf(text)
+        intermediateResults = mutableListOf(text)
         for (item in lstTranformItems) {
             text = doTransform(text, item)
-            interimResults.add(text)
+            intermediateResults.add(text)
         }
-        interimIndex.value = 0
-        interimMaxIndex = interimResults.size - 1
+        intermediateIndex.value = 0
+        intermediateMaxIndex = intermediateResults.size - 1
         resultText.value = text
         val t = vmDetail.template.value
         resultHtml = if (t.isEmpty()) toHtml(text) else applyTemplate(t, sourceWord.value, text)
+    }
+
+    fun reindex(onNext: (Int) -> Unit) {
+        for (i in 1..lstTranformItems.size) {
+            val item = lstTranformItems[i - 1]
+            if (item.index == i) continue
+            item.index = i
+            onNext(i - 1)
+        }
     }
 
 }
